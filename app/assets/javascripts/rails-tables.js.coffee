e7b55ecf09b27de8a210f1618045ba20fad476f9
Chris Keele <dev@chriskeele.com>
2012-12-10 18:29:00 -0600

    Start initial ordering [breaking]
    Signed-off-by: Chris Keele <dev@chriskeele.com>

--- app/assets/javascripts/rails-tables.js.coffee
+++ app/assets/javascripts/rails-tables.js.coffee
@@ -5,11 +5,12 @@ $ ->
     $(@).data('unsorted', true)
 
 @rails_tables = {}
-@rails_tables.columns = ->
-  for column, order of $(@).data() when /_ordering/.test(column)
-    asSorting: [ order ], aTargets: [ column.substring(0, column.length - "_ordering".length) ]
+@rails_tables.columns = (datatable)->
+  for column, order of $(datatable).data() when /_ordering/.test(column)
+    results = {asSorting: [ order ], aTargets: [ column.substring(0, column.length - "_ordering".length) ]}
+    results
 
-@rails_tables.params = (aoData) ->
-  aoData.push
-    name: 'bUseDefaultSort', value: if $(@).data('unsorted') then true else false
-
+@rails_tables.params = (datatable) ->
+  (aoData) ->
+    aoData.push
+      name: 'bUseDefaultSort', value: if $(datatable).data('unsorted') then true else false
--- lib/rails-tables/datatable/sorting.rb
+++ lib/rails-tables/datatable/sorting.rb
@@ -2,19 +2,19 @@ module RailsTables::Sorting
   extend ActiveSupport::Concern
 
   included do
-    class_attribute :initial_orderings
+    class_attribute :initial_ordering
     extend ClassMethods
   end
 
   module ClassMethods
     # Gotta set these to fight against datatables' default ordering on first column
-    def initial_ordering(orderings)
-      self.set_initial_orderings orderings
-    end
-    def set_initial_orderings(orderings)
-      self.initial_orderings = {} if self.initial_orderings.nil?
-      self.initial_orderings.merge! orderings
-    end
+    # def initial_ordering(orderings)
+    #   self.set_initial_orderings orderings
+    # end
+    # def set_initial_orderings(orderings)
+    #   self.initial_orderings = {} if self.initial_orderings.nil?
+    #   self.initial_orderings.merge! orderings
+    # end
 
   end
 
@@ -22,13 +22,23 @@ module RailsTables::Sorting
 private
   # Check if table is sortable
   def sortable
-    self.columns.map{ |column| column.sortable }[params[:iSortCol_0].to_i] unless params[:bUseDefaultSort] == 'true'
+    if params[:bUseDefaultSort] == 'true'
+      true
+    else
+      self.columns.map(&:sortable)[params[:iSortCol_0].to_i]
+    end
   end
   # Find column to search by and create a Squeel Order
   def sort
-    column = self.columns[params[:iSortCol_0].to_i]
-    if column.sortable
+    binding.pry
+    if params[:bUseDefaultSort] == 'true'
+      column = self.columns.select{|c|c.name == self.initial_ordering.keys.first.to_s}.first
+      directions = self.initial_ordering.values.first.to_s == "asc" ? 1 : -1
+    else
+      column = self.columns[params[:iSortCol_0].to_i]
       direction = params[:sSortDir_0] == "asc" ? 1 : -1
+    end
+    if column.sortable
       Squeel::Nodes::KeyPath.new(column.column_source.split('.') << Squeel::Nodes::Order.new(Squeel::Nodes::Stub.new(column.method), direction))
     end
   end
